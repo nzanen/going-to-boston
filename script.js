ce8f3e771496be2b5c4e82752dfa573c606a15d0
Niels <nzanen@gmail.com>
2023-01-18 19:04:23 +0100

    Tracking score per different roll

--- script.js
+++ script.js
@@ -29,37 +29,100 @@ function dieRoll() {
   return result;
 }
 
+let rollNumber = 1;
+
 btnRoll.addEventListener('click', function () {
   // Remove previous highlight
   die1.classList.remove('highlight');
   die2.classList.remove('highlight');
   die3.classList.remove('highlight');
 
-  // Roll dice and log results
-  const dieRoll1 = dieRoll();
-  const dieRoll2 = dieRoll();
-  const dieRoll3 = dieRoll();
-  const rollResult = [dieRoll1, dieRoll2, dieRoll3];
-  const highestRoll = Math.max(...rollResult);
-  const highestRollDie = rollResult.indexOf(highestRoll) + 1;
-  console.log(`Die rolls: ${rollResult}`);
-  console.log(`Highest roll: ${highestRoll}`);
-  console.log(`Die with highest roll: #${highestRollDie}`);
+  if (rollNumber === 1) {
+    // Roll dice and log results
+    const dieRoll1 = dieRoll();
+    const dieRoll2 = dieRoll();
+    const dieRoll3 = dieRoll();
+    const rollResult = [dieRoll1, dieRoll2, dieRoll3];
+    const highestRoll = Math.max(...rollResult);
+    const highestRollDie = rollResult.indexOf(highestRoll) + 1;
+    console.log(`Die rolls: ${rollResult}`);
+    console.log(`Highest roll: ${highestRoll}`);
+    console.log(`Die with highest roll: #${highestRollDie}`);
 
-  // Show result dice
-  die1.src = `dice-${dieRoll1}.png`;
-  die2.src = `dice-${dieRoll2}.png`;
-  die3.src = `dice-${dieRoll3}.png`;
+    // Show result dice
+    die1.src = `dice-${dieRoll1}.png`;
+    die2.src = `dice-${dieRoll2}.png`;
+    die3.src = `dice-${dieRoll3}.png`;
 
-  // Highlight highest dice roll
-  document.getElementById(`die${highestRollDie}`).classList.add('highlight');
+    // Highlight highest dice roll
+    document.getElementById(`die${highestRollDie}`).classList.add('highlight');
 
-  // Added score to current roll score
-  p0Score1 = highestRoll;
-  p0TotalScore += highestRoll;
+    // Added score to current roll score
 
-  p0Score1El.textContent = p0Score1;
-  p0TotalScoreEl.textContent = p0TotalScore;
+    p0Score1 = highestRoll;
+    p0TotalScore += highestRoll;
+
+    document.querySelector(`.p0Score${rollNumber}`).textContent = highestRoll;
+    p0TotalScoreEl.textContent = p0TotalScore;
+    // Increase roll number
+    rollNumber += 1;
+  } else if (rollNumber === 2) {
+    const dieRoll1 = dieRoll();
+    const dieRoll2 = dieRoll();
+    const dieRoll3 = dieRoll();
+    const rollResult = [dieRoll1, dieRoll2, dieRoll3];
+    const highestRoll = Math.max(...rollResult);
+    const highestRollDie = rollResult.indexOf(highestRoll) + 1;
+    console.log(`Die rolls: ${rollResult}`);
+    console.log(`Highest roll: ${highestRoll}`);
+    console.log(`Die with highest roll: #${highestRollDie}`);
+
+    // Show result dice
+    die1.src = `dice-${dieRoll1}.png`;
+    die2.src = `dice-${dieRoll2}.png`;
+    die3.src = `dice-${dieRoll3}.png`;
+
+    // Highlight highest dice roll
+    document.getElementById(`die${highestRollDie}`).classList.add('highlight');
+
+    // Added score to current roll score
+    p0Score2 = highestRoll;
+    p0TotalScore += highestRoll;
+
+    p0Score2El.textContent = p0Score2;
+    p0TotalScoreEl.textContent = p0TotalScore;
+
+    // Increase roll number
+    rollNumber += 1;
+  } else if (rollNumber === 3) {
+    const dieRoll1 = dieRoll();
+    const dieRoll2 = dieRoll();
+    const dieRoll3 = dieRoll();
+    const rollResult = [dieRoll1, dieRoll2, dieRoll3];
+    const highestRoll = Math.max(...rollResult);
+    const highestRollDie = rollResult.indexOf(highestRoll) + 1;
+    console.log(`Die rolls: ${rollResult}`);
+    console.log(`Highest roll: ${highestRoll}`);
+    console.log(`Die with highest roll: #${highestRollDie}`);
+
+    // Show result dice
+    die1.src = `dice-${dieRoll1}.png`;
+    die2.src = `dice-${dieRoll2}.png`;
+    die3.src = `dice-${dieRoll3}.png`;
+
+    // Highlight highest dice roll
+    document.getElementById(`die${highestRollDie}`).classList.add('highlight');
+
+    // Added score to current roll score
+    p0Score3 = highestRoll;
+    p0TotalScore += highestRoll;
+
+    p0Score3El.textContent = p0Score3;
+    p0TotalScoreEl.textContent = p0TotalScore;
+
+    // Reset roll number
+    rollNumber = 1;
+  }
 });
 
 btnNew.addEventListener('click', function () {
